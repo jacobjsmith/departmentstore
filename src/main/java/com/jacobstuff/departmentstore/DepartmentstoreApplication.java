0cd35d665464c451e6336239cae5a26e017e3b31
Jacob Smith <jacobsmith@Jacobs-MacBook-Pro.local>
2021-03-24 17:38:53 -0400

    First commit

---
+++ src/main/java/com/jacobstuff/departmentstore/DepartmentstoreApplication.java
@@ -0,0 +1,13 @@
+package com.jacobstuff.departmentstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DepartmentstoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DepartmentstoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jacobstuff/departmentstore/DepartmentstoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jacobstuff.departmentstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DepartmentstoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
